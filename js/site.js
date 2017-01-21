f3cbd9a3f26e1321cbf26395afbfe73bd4fef684
Mario <mario@techmunchies.net>
2017-01-21 15:35:30 -0500

    Added styling to popover, fixed body height bug

--- js/site.js
+++ js/site.js
@@ -17,3 +17,23 @@ $(() => {
     // Initialize and animate site name
     new Letters($('.site-name')[0], options).show();
 });
+
+// Display Background Picture Info when Info Button is Clicked
+$(document).ready(() => {
+
+    // If the popover isn't being displayed, display it. If it is displayed, undisplay it
+    $('.info-container').on('click', (e) => {
+        e.stopPropagation();
+        $('.info-popover').hasClass('info-popover-visible') ? $('.info-popover').removeClass('info-popover-visible') : $('.info-popover').addClass('info-popover-visible');
+    });
+
+    // Closes popover if clicking anywhere in the site
+    $('body').on('click', () => {
+        $('.info-popover').hasClass('info-popover-visible') ? $('.info-popover').removeClass('info-popover-visible') : true;
+    });
+
+    // Prevents closing popover when clicking on it
+    $('.info-popover').on('click', (e) => {
+        e.stopPropagation();
+    });
+});
