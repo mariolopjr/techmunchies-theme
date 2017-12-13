f5363d1f65e00f6e8a509e6e046e61c8d3011820
Mario Lopez <mario@techmunchies.net>
2017-12-12 22:48:18 -0500

    Switching to cards instead, more visually appealing and gets an image on there too

--- techmunchies/index.php
+++ techmunchies/index.php
@@ -1,5 +1,5 @@
 <?=get_header()?>
-<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+<?php query_posts('post_type=post&post_status=publish&posts_per_page=12&paged='. get_query_var('paged')); ?>
 
 <body class="blog">
   <?php require_once get_template_directory() . '/nav.php'; ?>
@@ -7,26 +7,39 @@
   <div class="content">
     <?php if( have_posts() ): ?>
       <?php while( have_posts() ): the_post(); ?>
-  	    <div id="post-<?=get_the_ID()?>" <?php post_class(); $categories = get_the_category(); ?>>
-          <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <br>
-          <span class="meta">
-            <?php if ( ! empty( $categories ) ) : ?>
-              <?='<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>'?>
-            <?php endif; ?>
-               ❖ <strong><?=the_time('m-d-Y')?></strong>
-          </span>
-
-  		<?php the_excerpt(__('Continue reading »','example')); ?>
-              </div>
-          <?php endwhile; ?>
+        <?php if ( ( $wp_query->current_post ) % 3 == 0 || $wp_query->current_post == 0): ?>
+          <div class="card-deck">
+        <?php endif; ?>
+        <div id="post-<?=get_the_ID()?>" class="card">
+          <img class="card-img-top" src="http://via.placeholder.com/350x200" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title"><a href="<?=the_permalink()?>"><?=the_title()?></a></h4>
+            <p class="card-text"><?=the_excerpt()?></p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">
+              <?php if ( ! empty( $categories = get_the_category() ) ) : ?>
+                <?='<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>'?>
+              <?php endif; ?>
+            </small>
+            |
+            <small class="text-muted">
+              5 MIN READ
+            </small>
+          </div>
+        </div>
+        <?php if ( ( $wp_query->current_post + 1 ) % 3 == 0 || $wp_query->current_post == 12): ?>
+        </div>
+        <?php endif; ?>
+      <?php endwhile; ?>
+      </div>
   		<div class="navigation">
   			<span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
   		</div>
   	<?php else: ?>
   		<div id="post-404" class="noposts">
   		    <p><?php _e('None found.','example'); ?></p>
-  	    </div>
+	    </div>
   	<?php endif; wp_reset_query(); ?>
   </div>
 
